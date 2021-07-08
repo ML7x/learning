e4ae9b3cead437ae1b191401a79cfced736f375b
Jab4r <jab4r@hotmail.com>
2021-07-08 12:49:33 +0300

    skeleton

---
+++ main.js
@@ -0,0 +1,6 @@
+// SEARCH BUTTON ON CLICK MAKE SEARCH ICON ORANGE
+document.getElementById('search-button').onclick = function(){document.getElementById('search-icon').classList.toggle('orange')}
+// MOON BUTTON ON CLICKED ADD LIGHT CLASS TO HTML
+document.getElementById('moon').onclick = function(){document.documentElement.classList.toggle('light')}
+// BARS BUTTON ON CLICK --FOR NOW ALERT - LATER ADD FUNC
+document.getElementById('bars').onclick = function(){alert('func here')}
