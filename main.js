46041b0375fe5e3f3ab2a9d1e62ea0da1a552c13
Jab4r <jab4r@hotmail.com>
2021-07-08 13:45:48 +0300

    a

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-// SEARCH BUTTON ON CLICK MAKE SEARCH ICON ORANGE
-document.getElementById('search-button').onclick = function(){document.getElementById('search-icon').classList.toggle('orange')}
+// SEARCH BUTTON ON CLICK MAKE SEARCH ICON ORANGE AND BOUNCE
+document.getElementById('search-button').onclick = function(){document.getElementById('search-icon').classList.toggle('orange');document.getElementById('search-icon').classList.toggle('bounce')}
 // MOON BUTTON ON CLICKED ADD LIGHT CLASS TO HTML
 document.getElementById('moon').onclick = function(){document.documentElement.classList.toggle('light')}
 // BARS BUTTON ON CLICK --FOR NOW ALERT - LATER ADD FUNC
-document.getElementById('bars').onclick = function(){alert('func here')}
+document.getElementById('bars').onclick = function(){alert('افتح الخيارات')}
